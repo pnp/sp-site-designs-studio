8ac59978a692bd30084808cb56510b9ae2d2177d
Richard Gigan <richard.gigan@fr.clara.net>
2021-08-30 19:33:19 +0200

    [new] - webpack-bundle-analyzer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const gulp = require('gulp');
+const path = require('path');
 const build = require('@microsoft/sp-build-web');
+const fs = require('fs');
+const bundleAnalyzer = require('webpack-bundle-analyzer');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 build.addSuppression(`Warning - [sass] src/styling/theme.scss: filename should end with module.sass or module.scss`);
@@ -13,6 +17,37 @@ build.rig.getTasks = function () {
 
   return result;
 };
+
+build.configureWebpack.mergeConfig({
+  additionalConfiguration: (generatedConfiguration) => {
+
+    // Bundle analyser (à decommneter si besoin)
+
+    const lastDirName = path.basename(__dirname);
+    const dropPath = path.join(__dirname, 'temp', 'stats');
+    generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+      generateStatsFile: true,
+      statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+      logLevel: 'error'
+    }));
+
+
+    // Alias
+    if (!generatedConfiguration.resolve.alias) {
+      generatedConfiguration.resolve.alias = {};
+    }
+    generatedConfiguration.resolve.alias['@services'] = path.resolve(__dirname, 'lib/services/');
+    generatedConfiguration.resolve.alias['@models'] = path.resolve(__dirname, 'lib/models/');
+    generatedConfiguration.resolve.alias['@helpers'] = path.resolve(__dirname, 'lib/helpers/');
+    generatedConfiguration.resolve.alias['@src'] = path.resolve(__dirname, 'lib');
+
+
+    return generatedConfiguration;
+  }
+});
 
 /* fast-serve */
 const { addFastServe } = require("spfx-fast-serve-helpers");
